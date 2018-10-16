753da9b29213fb446a5ed47776a96c074e9a9305
Mahnoor25 <44057172+Mahnoor25@users.noreply.github.com>
2018-10-16 11:57:54 -0700

    Add files via upload

---
+++ zombie.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include<stdio.h>
+
+int main()
+{
+    int id;
+    id=fork();
+
+    if(id>0)
+    {
+        printf("Parent will sleep");
+        sleep(10);
+    }
+    if(id==0)
+        printf("I am child");
+}
